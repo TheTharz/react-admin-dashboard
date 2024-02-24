b30dcb5cdc023c49054383da657b2fac4499501b
Tharindu Jayawardhana <tharinduimalka915@gmail.com>
2024-02-24 14:40:59 +0530

    theme all configured to light and dark

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 function App() {
-  return <div className='app'></div>;
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider value={theme}>
+        <CssBaseline>
+          <div className='app'></div>;
+        </CssBaseline>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
--- src/theme.js
+++ src/theme.js
@@ -123,4 +123,93 @@ export const tokens = (mode) => ({
 //mui theme settings
 export const themeSettings = (mode) => {
   const colors = tokens(mode);
+  return {
+    palette: {
+      mode: mode,
+      ...colors(
+        mode === 'dark'
+          ? {
+              primary: {
+                main: colors.primary[500],
+              },
+              secondary: {
+                main: colors.greenAccent[500],
+              },
+              neutral: {
+                main: colors.gray[500],
+                dark: colors.gray[700],
+                light: colors.gray[100],
+              },
+              background: {
+                default: colors.primary[500],
+              },
+            }
+          : {
+              primary: {
+                main: colors.primary[100],
+              },
+              secondary: {
+                main: colors.greenAccent[500],
+              },
+              neutral: {
+                main: colors.gray[500],
+                dark: colors.gray[700],
+                light: colors.gray[100],
+              },
+              background: {
+                default: '#fcfcfc',
+              },
+            }
+      ),
+    },
+    typography: {
+      fontFamily: ['Source Sans 3', 'sans-serif'].join(','),
+      fontSize: 12,
+      h1: {
+        fontFamily: ['Source Sans 3', 'sans-serif'].join(','),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ['Source Sans 3', 'sans-serif'].join(','),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ['Source Sans 3', 'sans-serif'].join(','),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ['Source Sans 3', 'sans-serif'].join(','),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ['Source Sans 3', 'sans-serif'].join(','),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ['Source Sans 3', 'sans-serif'].join(','),
+        fontSize: 14,
+      },
+    },
+  };
+};
+
+//context for color mode
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+export const useMode = () => {
+  const [mode, setMode] = useState('dark');
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () =>
+        setMode((prev) => (prev === 'light' ? 'dark' : 'light')),
+    }),
+    []
+  );
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return [theme, colorMode];
 };
